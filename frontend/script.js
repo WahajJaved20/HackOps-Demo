83dc7eaba5f2f9472770d9d8072de38bd76d74bd
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2025-04-28 18:32:15 +0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/data')
+fetch('http://node-backend:3000/data')
   .then(response => response.json())
   .then(data => {
     const list = document.getElementById('items-list');
